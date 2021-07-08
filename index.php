2f3ac2b10b3a7003d7ef8315d1601471995cd6ec
devnazmul <md.nazmul.islam1332@gmail.com>
2021-07-08 17:00:00 +0600

    create aside and mssage theme part

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,208 +8,19 @@
     <title>Four Arms Tech Community</title>
     <!-- tailwind css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/0.0.0-insiders.4a070ac/tailwind.min.css" integrity="sha512-vJu7D5BpjnNXVpLBrl9LKLvmXBNjiLwge8EOZ/YS9XwiChpfKLAlydwIZvoJaDE3LI/kr3goH0MzDzNbBgyoOQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
     <!-- main css -->
     <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
-<body>
-    <aside class="wrap w-full bg-gray-800">
-        <div class=" overflow-hidden sidebar w-full h-50 md:w-2/5 md:h-full lg:w-1/3 xl:w-1/4 2xl:w-1/6 bg-gray-900  rounded-tr-xl rounded-br-xl">
-            <div class="w-full h-20 px-9 flex items-center">
-                <div>
-                    <div class="mt-2">
-                        <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                        <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                    </div>
-                </div>
-                <div>
-                    <h2 class="text-white font-bold">Md Nazmul Islam</h2>
-                    <span class="text-white lg\:font-light text-gray-600 text-xs">Active Now</span>
-                </div>
-                
-            </div>
-            <hr class="border-gray-700 w-5/6 mx-auto">
-            <form class="w-full h-12 mx-5 my-5 flex">
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 rounded-l border-none outline-none bg-gray-800 py-3 px-4 mb-3 " id="grid-first-name" type="text" placeholder="Jane">
-                <input class="mr-10 h-9 bg-gray-800 rounded-r px-3" value="🔎" type="submit">
-            </form>
-            <div class="md:overflow-y-scroll h-full">
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-gray-800 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-                <div class="w-full h-20 px-9 flex items-center">
-                    <div>
-                        <div class="mt-2">
-                            <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
-                            <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
-                        <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <div class="">
-            <div></div>
-            <div></div>
-        </div>
-    </aside>
+<body>
+    <main class="wrap w-full bg-gray-800">
+        <?php
+            include "./theme_part/aside.php";
+            include "./theme_part/message.php";
+        ?>
+    </main>
 </body>
+
 </html>
---
+++ theme_part/aside.php
@@ -0,0 +1,190 @@
+<aside class="pr-1 overflow-hidden sidebar w-full h-50 md:w-2/5 md:h-full lg:w-1/3 xl:w-1/4 2xl:w-1/6 bg-gray-900  rounded-tr-xl rounded-br-xl">
+    <div class="w-full h-20 px-9 flex items-center">
+        <div>
+            <div class="mt-2">
+                <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+            </div>
+        </div>
+        <div>
+            <h2 class="text-white font-bold">Md Nazmul Islam</h2>
+            <span class="text-white lg\:font-light text-gray-600 text-xs">Active Now</span>
+        </div>
+
+    </div>
+    <hr class="border-gray-700 w-5/6 mx-auto">
+    <form class="w-full h-12 mx-5 my-5 flex">
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 rounded-l border-none outline-none bg-gray-800 py-3 px-4 mb-3 " id="grid-first-name" type="text" placeholder="Jane">
+        <input class="mr-10 h-9 bg-gray-800 rounded-r px-3" value="🔎" type="submit">
+    </form>
+    <div class="md:overflow-y-scroll h-full">
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-gray-800 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+        <div class="w-full h-20 px-9 flex items-center">
+            <div>
+                <div class="mt-2">
+                    <img class="rounded-full mr-3 h-14 w-auto border-4 border-gray-700" src="./assets/img/profile_me.jpg" alt="">
+                    <div class="bg-green-500 border-2 border-gray-700 h-4 w-4 rounded-full float-right relative bottom-4 right-3"></div>
+                </div>
+            </div>
+            <div>
+                <h2 class="text-gray-500 font-bold">Md Nazmul Islam</h2>
+                <span class="text-white lg\:font-light text-gray-700 text-xs">Hello how Are You........</span>
+            </div>
+        </div>
+    </div>
+</aside>
