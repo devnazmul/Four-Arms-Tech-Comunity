5148988fdc124e219a5ce23b66db0443ec47019d
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2021-07-25 16:14:41 +0600

    mobile compatible list.js ajax

---
+++ ajux/list.js
@@ -0,0 +1,67 @@
+//caht list ajax
+const userList = document.querySelector('#userList'),
+    searchInput = document.querySelector('#searchInput'),
+    searchBtn = document.querySelector('#searchBtn');
+
+searchInput.onkeyup = () => {
+    var searchTerm = searchInput.value;
+
+    if (searchTerm != "") {
+        searchInput.classList.add('searchActive');
+    } else {
+        searchInput.classList.remove('searchActive');
+    }
+    
+    if (window.innerWidth < 640) {
+        $.ajax({
+            url: "actions/mobile_search.php",
+            type: "POST",
+            data: {
+                search_term: searchTerm
+            },
+            success: function (data) {
+                userList.innerHTML = data;
+            }
+        });
+    } else {
+        $.ajax({
+            url: "actions/search.php",
+            type: "POST",
+            data: {
+                search_term: searchTerm
+            },
+            success: function (data) {
+                userList.innerHTML = data;
+            }
+        });
+    }
+    
+}
+
+if (window.innerWidth < 640) {
+    setInterval(() => {
+        $.ajax({
+            url: "actions/mobile_list.php",
+            type: "POST",
+
+            success: function (data) {
+                if (!searchInput.classList.contains('searchActive')) {
+                    userList.innerHTML = data;
+                }
+            }
+        });
+    }, 500);
+} else {
+    setInterval(() => {
+        $.ajax({
+            url: "actions/list.php",
+            type: "POST",
+
+            success: function (data) {
+                if (!searchInput.classList.contains('searchActive')) {
+                    userList.innerHTML = data;
+                }
+            }
+        });
+    }, 500);
+}
