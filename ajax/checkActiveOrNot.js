46239c93c7ad7bd9c1d8120c8570304ef2c69a47
devnazmul <md.nazmul.islam1332@gmail.com>
2021-07-28 22:08:43 +0600

    not a big changes

--- ajax/checkActiveOrNot.js
+++ ajax/checkActiveOrNot.js
@@ -2,7 +2,7 @@ setInterval(() => { // check user active or not
     $.ajax({
         url: "actions/checkActiveOrNot.php",
         success: function (data) {
-            
+            $('#my_profile').html(data);
         }
     });
-}, 500);
+}, 5);
